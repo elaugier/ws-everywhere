906072987d7e70b5a069ad6e9204ca4566a555e6
Eric LAUGIER <eric.laugier@egen.guru>
2019-10-11 07:49:30 +0200

    update release

---
+++ install-service.cmd
@@ -0,0 +1,18 @@
+@echo off
+whoami /groups | findstr /c:" S-1-5-32-544 " | findstr /c:" Enabled group" && goto :isadministrator
+whoami /groups | findstr /c:" S-1-5-32-544 " | findstr /c:" Groupe activ" && goto :isadministrator
+@echo This script requires elevated rights.
+exit /b 1
+:isadministrator
+if [%1] == [] goto usage
+set PATH=%~dp0python;%~dp0cmake\bin;%PATH%
+nssm install "%1" "%~dp0nginx.exe"
+set addpaths=%~dp0python;%~dp0cmake\bin;
+@echo %addpaths%
+nssm set "%1" AppEnvironmentExtra PATH=addpaths^%PATH^%
+python --version
+cmake --version
+goto :eof
+:usage
+@echo Usage: %~n0%~x0 ^<ServiceName^>
+exit /b 1
